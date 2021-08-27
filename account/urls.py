5feae3c22abdc273b1f94dd65b4dd3b0a45f7593
G0udini <ruslan.shabunin2010@yandex.ru>
2021-08-27 10:49:37 +0300

    add registration page and requirements

--- account/urls.py
+++ account/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path, include
+
 # from django.contrib.auth.views import (
 #     LoginView,
 #     LogoutView,
@@ -10,10 +11,11 @@ from django.urls import path, include
 #     PasswordResetCompleteView,
 # )
 
-from .views import dashboard
+from .views import dashboard, register
 
 # app_name = "account"
 urlpatterns = [
+    path("register/", register, name="register"),
     path("", include("django.contrib.auth.urls")),
     path("dashboard/", dashboard, name="dashboard"),
 ]
